d50c9b584026ba6ce50b091722bc44d3cf262819
Nazivon <145787309+Nazivon@users.noreply.github.com>
2025-10-26 19:24:43 -0300

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
 
   # Validações:
   validates :name, presence: true
+  validates :preco, numericality: { greater_than_or_equal_to: 0 }
 
   # Adiciona itens sem tag ao final.
   scope :grouped_by_tag, -> { order(Arel.sql("CASE WHEN tag IS NULL THEN 1 ELSE 0 END, tag ASC")) }
