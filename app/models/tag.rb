20aeb038343a2c2331490fd84eb7a22a058ddda2
Nazivon <145787309+Nazivon@users.noreply.github.com>
2025-10-26 08:18:51 -0300

    Update tag.rb

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,9 @@ class Tag < ApplicationRecord
   has_many :taggings
   has_many :items, through: :taggings
   validates :name, presence: true, uniqueness: true
+
+  # Lógica para obter tags pré-cadastradas
+  def self.pre_registered
+    all
+  end
 end
