4c05c1fb3c7e8517f7399132a9aed43f4e6e069a
Nazivon <145787309+Nazivon@users.noreply.github.com>
2025-10-26 08:28:47 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+   # 1. Rota para o recurso 'Item'
+  resources :items, path: 'lista' do
+    # Rota para a ação de ordenação/filtragem
+    collection do
+      get :sort
+    end
+  end
+
   # Defines the root path route ("/")
   root "home#index" # <-- ESSA É A ÚNICA LINHA QUE VOCÊ PRECISA PARA A PÁGINA INICIAL
 end
