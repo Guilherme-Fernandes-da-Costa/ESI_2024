ca3d0afc8d767387b18030f5ba756cd6f8e051a9
Nazivon <145787309+Nazivon@users.noreply.github.com>
2025-10-26 17:55:58 -0300

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,20 +1,8 @@
 # app/models/item.rb
 class Item < ApplicationRecord
+  # Associações:
   belongs_to :list
-  belongs_to :added_by, class_name: 'User'
-  
-  has_many :taggings
-  has_many :tags, through: :taggings
-  validates :name, presence:true
 
-  def self.with_tag(tag_name)
-    joins(:tags).where(tags: { name: tag_name })
-  end
-
-  # Escopo para ordenar itens pela tag (necessário para a função 'Agrupar')
-  # Assumes que itens sem tag devem ser excluídos na ordenação por tag
-  scope :sorted_by_tag, -> { joins(:tag).order('tags.name ASC') }
-  
-  # Escopo para filtrar itens por tag (necessário para a função 'Filtrar')
-  scope :filter_by_tag, ->(tag_id) { where(tag_id: tag_id) }
+  # Validações:
+  validates :name, presence: true
 end
