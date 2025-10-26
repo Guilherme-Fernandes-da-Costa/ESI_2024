e39b04495be30f633f6566f11841fda98e466813
Nazivon <145787309+Nazivon@users.noreply.github.com>
2025-10-26 18:37:16 -0300

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,12 @@ class Item < ApplicationRecord
 
   # Adiciona itens sem tag ao final.
   scope :grouped_by_tag, -> { order(Arel.sql("CASE WHEN tag IS NULL THEN 1 ELSE 0 END, tag ASC")) }
+
+  def marcar_como_comprado
+    update(comprado: true) unless comprado?
+  end
+
+  def desmarcar_como_comprado
+    update(comprado: false) if comprado?
+  end
 end
