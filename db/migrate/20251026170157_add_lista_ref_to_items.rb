f56b2c12fcfd68233efceca15bb30002b0300ddd
Nazivon <nxivon@DESKTOP-8I14Q99.localdomain>
2025-10-26 14:07:48 -0300

    Adiciona migração de adicionar lista ref a itens

---
+++ db/migrate/20251026170157_add_lista_ref_to_items.rb
@@ -0,0 +1,5 @@
+class AddListaRefToItems < ActiveRecord::Migration[7.1]
+  def change
+    add_reference :items, :lista, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_26_115320) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_26_124529) do
   create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "list_id"
+    t.integer "added_by_id"
+    t.index ["added_by_id"], name: "index_items_on_added_by_id"
+    t.index ["list_id"], name: "index_items_on_list_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_taggings_on_item_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "items", "lists"
+  add_foreign_key "items", "users", column: "added_by_id"
+  add_foreign_key "taggings", "items"
+  add_foreign_key "taggings", "tags"
 end
