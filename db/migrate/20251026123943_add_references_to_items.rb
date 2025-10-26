0022e0cdf99065ea32d6fb87761d6bb118eab2d8
Nazivon <145787309+Nazivon@users.noreply.github.com>
2025-10-26 09:43:46 -0300

    Update 20251026123943_add_references_to_items.rb

--- db/migrate/20251026123943_add_references_to_items.rb
+++ db/migrate/20251026123943_add_references_to_items.rb
@@ -1,6 +1,6 @@
-class AddReferencesToItems < ActiveRecord::Migration[7.1]
+class AddListAndAddedByToItems < ActiveRecord::Migration[7.1]
   def change
-    add_reference :items, :list, null: false, foreign_key: true
-    add_reference :items, :added_by, null: false, foreign_key: true
+    add_reference :items, :list, foreign_key: true
+    add_reference :items, :added_by, foreign_key: { to_table: :users }
   end
 end
