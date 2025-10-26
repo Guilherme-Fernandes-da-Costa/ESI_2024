4349de24d341fec06f7eebc42a456710343a5d9b
Nazivon <145787309+Nazivon@users.noreply.github.com>
2025-10-26 18:02:46 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index" # <-- ESSA É A ÚNICA LINHA QUE VOCÊ PRECISA PARA A PÁGINA INICIAL
 
-  resources :lists, controller: "lists", only: [:index, :show] do
-    resources :items do
-      collection do
-        get :filter
-        get :group
-      end
-    end
+  resources :lists do
+    resources :items, only: [:new, :create, :index]
   end
+  # Adicione uma rota para a raiz se necessário (ex: root to: 'lists#index')
+  get 'list/:id', to: 'lists#show', as: :list # Acesso direto à lista, conforme a step definition
 end
