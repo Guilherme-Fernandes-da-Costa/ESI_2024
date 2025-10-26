0e7d7a721f825565a126ac1f0fad642060d866b8
Nazivon <145787309+Nazivon@users.noreply.github.com>
2025-10-26 10:18:57 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   resources :items, path: 'lista' do
     # Rota para a ação de ordenação/filtragem
     collection do
-      get :sort
+      get 'sort'
     end
   end
 
