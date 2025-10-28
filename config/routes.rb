f3552f9908e74bd3814c76a8f57ab2a9c5378a70
danielye1234 <168474251+danielye1234@users.noreply.github.com>
2025-10-28 19:47:18 -0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  root "home#index" # <-- ESSA É A ÚNICA LINHA QUE VOCÊ PRECISA PARA A PÁGINA INICIAL
+    resources :lists do
+    post 'reset', on: :member
+    end
 end
