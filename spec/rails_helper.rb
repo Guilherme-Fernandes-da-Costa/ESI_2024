9f3113695581a35341e411efc6aac94b7962b247
Marcos Medeiros <marcos.mb.mb@outlook.com>
2025-11-02 17:30:47 -0300

    trabalho chato

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,6 +65,10 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+
+  config.include Rails.application.routes.url_helpers
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
