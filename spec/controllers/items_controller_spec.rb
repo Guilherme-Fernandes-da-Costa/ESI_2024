cfcfa27a9f4c6e16031f0416b1e173252dae7c79
Nazivon <145787309+Nazivon@users.noreply.github.com>
2025-11-02 17:01:54 -0300

    Update items_controller_spec.rb

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
-  let!(:lista) { Lista.create!(nome: "Minha Lista") }
+  let!(:list) { List.create!(nome: "Minha Lista") }
   let!(:tag1) { Tag.create!(name: "Urgente") }
   let!(:tag2) { Tag.create!(name: "Opcional") }
 
@@ -10,7 +10,7 @@ RSpec.describe ItemsController, type: :controller do
 
   describe "GET #new" do
     it "retorna sucesso" do
-      get :new, params: { lista_id: lista.id }
+      get :new, params: { list_id: list.id }
       expect(response).to have_http_status(:success)
     end
   end
@@ -19,47 +19,47 @@ RSpec.describe ItemsController, type: :controller do
     context "com atributos válidos" do
       it "cria um novo item" do
         expect {
-          post :create, params: { lista_id: lista.id, item: valid_attributes }
+          post :create, params: { list_id: list.id, item: valid_attributes }
         }.to change(Item, :count).by(1)
       end
 
       it "redireciona para a lista" do
-        post :create, params: { lista_id: lista.id, item: valid_attributes }
-        expect(response).to redirect_to(lista_path)
+        post :create, params: { list_id: list.id, item: valid_attributes }
+        expect(response).to redirect_to(list_path)
       end
     end
 
     context "com atributos inválidos" do
       it "não cria item" do
         expect {
-          post :create, params: { lista_id: lista.id, item: invalid_attributes }
+          post :create, params: { list_id: list.id, item: invalid_attributes }
         }.not_to change(Item, :count)
       end
 
       it "renderiza new novamente" do
-        post :create, params: { lista_id: lista.id, item: invalid_attributes }
+        post :create, params: { list_id: list.id, item: invalid_attributes }
         expect(response).to render_template(:new)
       end
     end
   end
 
   describe "PATCH #update" do
-    let!(:item) { Item.create!(name: "Item Antigo", list: lista) }
+    let!(:item) { Item.create!(name: "Item Antigo", list: list) }
 
     context "com atributos válidos" do
       it "atualiza o item" do
-        patch :update, params: { lista_id: lista.id, id: item.id, item: { name: "Item Atualizado" } }
+        patch :update, params: { list_id: list.id, id: item.id, item: { name: "Item Atualizado" } }
         expect(item.reload.name).to eq("Item Atualizado")
       end
     end
   end
 
   describe "DELETE #destroy" do
-    let!(:item) { Item.create!(name: "Item a remover", list: lista) }
+    let!(:item) { Item.create!(name: "Item a remover", list: list) }
 
     it "remove o item" do
       expect {
-        delete :destroy, params: { lista_id: lista.id, id: item.id }
+        delete :destroy, params: { list_id: list.id, id: item.id }
       }.to change(Item, :count).by(-1)
     end
   end
