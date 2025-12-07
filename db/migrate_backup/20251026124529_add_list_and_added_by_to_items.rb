f770a22ba6ccb30c081534e673be32fbf8d27664
MateusEsposte <esposte145A@gmail.com>
2025-12-07 15:34:47 -0300

    a

--- db/migrate_backup/20251026124529_add_list_and_added_by_to_items.rb
+++
@@ -1,6 +0,0 @@
-class AddListAndAddedByToItems < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :items, :list, foreign_key: true
-    add_reference :items, :added_by, foreign_key: { to_table: :users }
-  end
-end
