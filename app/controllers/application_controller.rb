89aa9ed728b7fe64dd602daafc05760253e59cb6
danielye1234 <168474251+danielye1234@users.noreply.github.com>
2025-12-08 07:40:59 -0300

    adiciona e corrigir friendship

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def authenticate_user!
+      redirect_to login_path, alert: "Você precisa estar logado para acessar esta página." unless logged_in?
+  end
 end
--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,30 +1,31 @@
+# app/controllers/friendships_controller.rb
 class FriendshipsController < ApplicationController
-    before_action :authenticate_user! 
+  before_action :authenticate_user!
 
-    def index
-        @friends = current_user.friendships.includes(:friend)
+  def index
+    @friends = current_user.friendships.includes(:friend)
+  end
+
+  def create
+    friend = User.find_by(email: params[:email])
+
+    if friend.nil?
+      redirect_back fallback_location: root_path, alert: "Usuário não encontrado."
+      return
     end
 
-    def create
-        friend = User.find_by(email: params[:email])
+    friendship = Friendship.new(user: current_user, friend: friend)
 
-        if friend.nil?
-            redirect_back fallback_location: root_path, alert: "Usuário não encontrado."
-            return
-        end
-
-        friendship = Friendship.new(user: current_user, friend: friend)
-
-        if friendship.save
-            redirect_back fallback_location: root_path, notice: "Amigo adicionado com sucesso!"
-        else
-            redirect_back fallback_location: root_path, alert: friendship.errors.full_messages.join(", ")
-        end
+    if friendship.save
+      redirect_back fallback_location: root_path, notice: "Amigo adicionado com sucesso!"
+    else
+      redirect_back fallback_location: root_path, alert: friendship.errors.full_messages.join(", ")
     end
+  end
 
-    def destroy
-        friendship = Friendship.find(params[:id])  # ← CORRIGIDO
-        friendship.destroy
-        redirect_back fallback_location: root_path, notice: "Amigo removido."
-    end
+  def destroy
+    friendship = Friendship.find(params[:id])  # ← Já corrigido
+    friendship.destroy
+    redirect_back fallback_location: root_path, notice: "Amigo removido."
+  end
 end
