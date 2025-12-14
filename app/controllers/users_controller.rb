98b48a73af16af6782a4885471eda16aa1a88d57
Nazivon <nazsnascimento@gmail.com>
2025-12-14 10:51:14 -0300

    Subindo alterações

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,7 @@ class UsersController < ApplicationController
 
     if @user.save
       session[:user_id] = @user.id
-<<<<<<< HEAD
       redirect_to lists_path, notice: "Conta criada com sucesso!"
-=======
-      redirect_to lists_path, notice: 'Conta criada com sucesso!'
->>>>>>> 0d98f6d (Implementa sistema de autenticação)
     else
       render :new, status: :unprocessable_entity
     end
@@ -24,8 +20,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 0d98f6d (Implementa sistema de autenticação)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 # config/routes.rb
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get "friendships/create"
   get "friendships/destroy"
   root "home#index"
@@ -12,17 +11,6 @@ Rails.application.routes.draw do
 
   get "signup", to: "users#new"
   post "signup", to: "users#create"
-=======
-  root "home#index"
-
-  # Rotas de autenticação
-  get 'login', to: 'sessions#new'
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
-
-  get 'signup', to: 'users#new'
-  post 'signup', to: 'users#create'
->>>>>>> 0d98f6d (Implementa sistema de autenticação)
 
   resources :lists do
     post "reset", on: :member
@@ -37,14 +25,9 @@ Rails.application.routes.draw do
   end
 
   # Rota para o BDD
-<<<<<<< HEAD
   get "lista", to: "lists#index"
 
   resources :friendships, only: [ :create, :destroy ]
 
   get "/friends", to: "friendships#index", as: "friends"
 end
-=======
-  get 'lista', to: 'lists#index'
-end
->>>>>>> 0d98f6d (Implementa sistema de autenticação)
